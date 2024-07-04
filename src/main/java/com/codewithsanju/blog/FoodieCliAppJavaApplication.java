940c5396acdba862935ae5c272b63e03213433be
Santhosh Reddy <reddysanju9898@gmail.com>
2024-07-04 12:41:17 +0530

    chore: intial commit

---
+++ src/main/java/com/codewithsanju/blog/FoodieCliAppJavaApplication.java
@@ -0,0 +1,13 @@
+package com.codewithsanju.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieCliAppJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodieCliAppJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewithsanju/blog/FoodieCliAppJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithsanju.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodieCliAppJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
